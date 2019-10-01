e9a17aa6027c8bfc04c9f3c5252dfd5c6521a170
Elaine Shannon <eshannon3@stmarytx.edu>
2019-10-01 16:19:25 -0500

    Step 2: The simplest part of the JavaScript

--- src/blocks/related-posts/index.js
+++ src/blocks/related-posts/index.js
@@ -1,13 +1,33 @@
 const { registerBlockType } = wp.blocks;
-
+import Block from './block';
+ 
 registerBlockType('my/related-posts', {
-    title: 'Related Posts',
+    title: 'Related Posts',     
+    category: 'widgets',
     icon: 'image-filter',
-    category: 'common',
-    edit: props => {
-        return <span>Editor</span>;
+    attributes: {
+        'editMode': {
+            type: 'boolean',
+            default: true
+        },
+        'postIds': {
+            type: 'array',
+            default: []
+        },
+        'postType': {
+            type: 'string',
+            default: 'posts'
+        },
+        'updated': {
+            type: 'string',
+            default: ''
+        }
     },
-    save: props => {
-        return <span>Front End</span>;
-    }
+    edit( props ) {
+        return <Block { ...props } />;
+    },
+    save() {
+        // Rendering in PHP
+        return null;
+    },
 } );
---
+++ src/blocks/related-posts/searchposts.js
@@ -0,0 +1,20 @@
+const { Button, MenuGroup, MenuItem, SelectControl, TextControl } = wp.components;
+const { Component, Fragment } = wp.element;
+ 
+export class SearchPostsControl extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div className='search-posts-control'>
+                <div className='posts-selected'>
+                    <h2>Currently selected:</h2>
+                </div>
+                <div className='posts-search'>
+                    <h2>Add to selections:</h2>
+                </div>
+            </div>
+        );
+    }
+}
